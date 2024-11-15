b9f91b8adfeb44f8b56cb325ed4c53e441c5ff4c
Denis Tarasenko <deniskatarasenko6@gmail.com>
2024-11-15 15:12:02 +0200

    Close mobile sidebar on click outside

--- app/components/Layout/MobileSidebar.tsx
+++ app/components/Layout/MobileSidebar.tsx
@@ -1,21 +1,48 @@
 "use client";
 
 import { Menu } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+const navItems = [
+  { name: "Get a Demo", href: "#" },
+  { name: "Contact", href: "#" },
+  { name: "Sign in", href: "#" },
+];
 
 function MobileSidebar() {
   const [open, setOpen] = useState(false);
+  const sidebarRef = useRef(null);
+  const sidebarButtonRef = useRef(null);
 
   const handleOpenSidebar = () => {
     setOpen((prev) => !prev);
   };
+
+  const handleClickOutside = (event: any) => {
+    if (
+      sidebarRef.current &&
+      sidebarButtonRef.current &&
+      !(sidebarButtonRef.current as any).contains(event.target) &&
+      !(sidebarRef.current as any).contains(event.target)
+    ) {
+      setOpen(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("click", handleClickOutside);
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="xl:hidden">
-      <button onClick={handleOpenSidebar}>
+      <button ref={sidebarButtonRef} onClick={handleOpenSidebar}>
         <Menu />
       </button>
 
       <div
+        ref={sidebarRef}
         className={`${open ? "flex" : "hidden"} absolute top-0 left-0 w-full`}
       >
         <div className="flex justify-end bg-white p-[20px] w-full">
@@ -26,9 +53,11 @@ function MobileSidebar() {
             >
               <Menu />
             </button>
-            <a href="#">Get a Demo</a>
-            <a href="#">Contact</a>
-            <a href="#">Sign in</a>
+            {navItems.map((item, i) => (
+              <a key={i} href="#">
+                {item.name}
+              </a>
+            ))}
           </div>
         </div>
       </div>
